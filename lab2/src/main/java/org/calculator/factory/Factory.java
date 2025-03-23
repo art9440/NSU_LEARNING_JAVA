9d82491e723de12dd51e3423b1899156be414201
art9440 <art9440@gmail.com>
2025-03-23 11:51:22 +0700

    Made so fix: Created mapping with map<String, Constructor> in Factory.java

--- lab2/src/main/java/org/calculator/factory/Factory.java
+++ lab2/src/main/java/org/calculator/factory/Factory.java
@@ -8,6 +8,7 @@ import org.calculator.exeptions.factoryExceptions.CreatingCommandException;
 import org.calculator.exeptions.factoryExceptions.FactoryException;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
@@ -17,7 +18,7 @@ import java.util.logging.Logger;
 public class Factory {
     private final String factoryConfig;
     private static final Logger logger = Logger.getLogger(Factory.class.getName());
-    private Map<String, String> factoryMap = new HashMap<>();
+    private Map<String, Constructor> factoryMap = new HashMap<>();
 
     public Factory(String factoryConfig) {
         this.factoryConfig = factoryConfig;
@@ -27,20 +28,33 @@ public class Factory {
             logger.severe("Error: " + e.getMessage());
             System.exit(1);
         }
+        catch (CommandNotFoundException | CreatingCommandException e){
+            logger.severe("Error:" + e.getMessage());
+            System.exit(1);
+        }
         logger.info("Factory is created");
     }
 
-    private void mappingConfig() throws ConfigFactoryNotFoundException, IOException {
+    private void mappingConfig() throws ConfigFactoryNotFoundException, IOException, CommandNotFoundException, CreatingCommandException {
         InputStream inputStream = getClass().getClassLoader().getResourceAsStream(factoryConfig);
         if (inputStream == null) {
             throw new ConfigFactoryNotFoundException("Can`t find factory config.");
         }
-
-        BufferedReader configFactoryReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
-        String line;
-        while ((line = configFactoryReader.readLine()) != null) {
-            String[] lineDiv = line.split("=");
-            factoryMap.put(lineDiv[0], lineDiv[1]);
+        try {
+            BufferedReader configFactoryReader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            String line;
+            while ((line = configFactoryReader.readLine()) != null) {
+                String[] lineDiv = line.split("=");
+                factoryMap.put(lineDiv[0], Class.forName(lineDiv[1]).getDeclaredConstructor(String[].class));
+            }
+        }
+        catch (ClassNotFoundException e){
+            logger.severe("Something went wrong while searching this class: " + e.getMessage());
+            throw new CommandNotFoundException(e.getMessage());
+        }
+        catch (NoSuchMethodException e){
+            logger.severe("Something went wrong in Creating command with using this class ->  " + e.getMessage());
+            throw new CreatingCommandException(e.getMessage());
         }
 
         inputStream.close();
@@ -51,26 +65,22 @@ public class Factory {
     public Command createCommand(String command, String[] arguments) throws FactoryException {
         logger.info("Process of creating command: " + command + " is started");
         try {
-            String pathToCommand = search(command);
-            logger.info("Path to command: " + command + " is: " + pathToCommand);
+            Constructor classConstructor = search(command);
 
 
-            return (Command) Class.forName(pathToCommand).getDeclaredConstructor(String[].class).newInstance((Object) arguments);
+            return (Command) classConstructor.newInstance((Object) arguments);
 
         }
-        catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e){
+        catch (InstantiationException | IllegalAccessException | InvocationTargetException e){
             logger.severe("Something went wrong in Creating command with using this class ->  " + e.getMessage());
             throw new CreatingCommandException(e.getMessage());
         }
-        catch (ClassNotFoundException e){
-            throw new CommandNotFoundException(e.getMessage());
-        }
     }
 
-    String search(String command) throws CommandNotFoundException {
+    private Constructor search(String command) throws CommandNotFoundException {
         logger.info("Searching path for command: " + command + " is started.");
 
-        String path = factoryMap.get(command);
+        Constructor path = factoryMap.get(command);
         if (path != null){
             return path;
         }
